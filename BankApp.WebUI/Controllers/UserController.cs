fc077dcfeaad5daf4203f0399415cd5daec4e69a
rickard uttermalm <rickard.uttermalm@gmail.com>
2019-05-27 12:21:22 +0200

    fixing navbar

---
+++ BankApp.WebUI/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankApp.Application.Interfaces;
+using BankApp.Persistence;
+using BankApp.WebUI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApp.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _usermanager;
+        private readonly SignInManager<ApplicationUser> _signinmanager;
+
+        public UserController(UserManager<ApplicationUser> usermanager, SignInManager<ApplicationUser> signinmanager)
+        {
+            _usermanager = usermanager;
+            _signinmanager = signinmanager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signinmanager.PasswordSignInAsync(user.Email, user.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["Logingfail"] = "Användarnamn eller lösenord stämmer inte";
+                }
+            }
+            return View(user);
+        }
+    }
+}
